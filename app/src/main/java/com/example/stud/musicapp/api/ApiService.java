8d9f349918e4105032ff42a9fd23f507a6b83897
Kamil Smołucha <w57132@poczta.wsiz.rzeszow.pl>
2018-05-12 13:28:33 +0200

    Modele

--- app/src/main/java/com/example/stud/musicapp/api/ApiService.java
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -12,4 +12,5 @@ public class ApiService {
 
         return retrofit.create(ApiClient.class);
     }
+
 }
---
+++ app/src/main/java/com/example/stud/musicapp/api/TrendingList.java
@@ -0,0 +1,8 @@
+package com.example.stud.musicapp.api;
+
+
+import java.util.List;
+
+public class TrendingList {
+    public List<TrendingSingle> trending ;
+}
