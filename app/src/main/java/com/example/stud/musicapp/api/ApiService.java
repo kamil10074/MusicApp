61f986462b81fb6714f95c9876322c602f370b78
Kamil <w57132@poczta.wsiz.rzeszow.pl>
2018-05-26 12:29:50 +0200

    Gson

--- app/src/main/java/com/example/stud/musicapp/api/ApiService.java
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -2,12 +2,14 @@ package com.example.stud.musicapp.api;
 
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiService {
 
     public static ApiClient getService() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://www.theaudiodb.com/api/v1/json/1/")
+                .baseUrl( "http://www.theaudiodb.com/api/v1/json/{APIKEY}/" )
+                .addConverterFactory(GsonConverterFactory. create ())
                 .build();
 
         return retrofit.create(ApiClient.class);
