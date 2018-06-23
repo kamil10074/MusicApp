528607381d09bcb7eb6f328c919314ad20338713
Kamil Smolucha <w57132@poczta.wsiz.rzeszow.pl>
2018-06-23 11:46:45 +0200

    Wyswietlanie szczegolow

--- app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
@@ -1,9 +1,21 @@
 package com.example.stud.musicapp.topsongs;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
+import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.Track;
+import com.example.stud.musicapp.api.Tracks;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.widget.Toast.*;
 
 public class SongDetailsActivity extends AppCompatActivity {
 
@@ -28,6 +40,36 @@ public class SongDetailsActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle(track);
         getSupportActionBar().setSubtitle(artist);
+        ApiService. getService ().getTrack(trackId).enqueue(new Callback<Tracks>() {
+            @Override
+            public void onResponse(@NonNull Call<Tracks> call, @NonNull Response<Tracks>
+                    response) {
+                Tracks tracks = response.body();
+                if (tracks != null && tracks. track .size() > 0 ) {
+                    showData(tracks. track .get( 0 ));
+                }
+            }
+            @Override
+            public void onFailure( @NonNull Call<Tracks> call, @NonNull Throwable t) {
+                makeText (
+                        SongDetailsActivity. this ,
+                        "Błąd pobierania danych: " + t.getLocalizedMessage(),
+                        LENGTH_SHORT
+                ).show();
+            }
+        });
+
+    }
+
+    private void showData(Track track) {
+        TextView tvAlbum = findViewById(R.id. tvAlbum );
+        TextView tvGenre = findViewById(R.id. tvGenre );
+        TextView tvStyle = findViewById(R.id. tvStyle );
+        TextView tvDescription = findViewById(R.id. tvDescription );
+        tvAlbum.setText(track. strAlbum );
+        tvGenre.setText(track. strGenre );
+        tvStyle.setText(track. strStyle );
+        tvDescription.setText(track. strDescriptionEN );
     }
     @Override
     public boolean onSupportNavigateUp() {
@@ -35,4 +77,6 @@ public class SongDetailsActivity extends AppCompatActivity {
 
         return true;
     }
+
+
 }
